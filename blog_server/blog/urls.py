2b365d2b1884ac8e78ac36e61251d311f0a16d6c
bushitan <373514952@qq.com>
2016-08-16 16:11:51 +0800

    v 1.3.1 作品->私密画廊

--- blog_server/blog/urls.py
+++ blog_server/blog/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
 
    url(r'^index/$', IndexView.as_view()),
    # url(r'^gallery/(?P<open_id>\w+)$', GalleryView.as_view()),
+
    url(r'^gallery/$', GalleryView.as_view()),
 
-   url(r'^artwork/$', ArtworkView.as_view()),
+   url(r'^artwork/$', ArtworkAddView.as_view()),
+   url(r'^artwork/show/$', ArtworkShowView.as_view()),
 
    url(r'^game/(?P<game_id>\w+)$', GameView.as_view()),
    url(r'^game/add/$', GameAddView.as_view()),
