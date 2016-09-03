4213b480352a806b16287701b7a3b63802bb9c0a
“bushitan” <373514952@qq.com>
2016-09-03 08:50:10 +0800

    v 2.2.2 重新配置pyw~~

--- blog_server/blog_server/wsgi.py
+++ blog_server/blog_server/wsgi.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+import sys
+# assuming your django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/bushitan1/blog_fx/blog_server'
+if path not in sys.path:
+    sys.path.append(path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_server.settings")
